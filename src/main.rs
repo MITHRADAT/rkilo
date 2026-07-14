894be7b687c1cf219e8b2cc0a93b6340cdd56e0e
MITHRADAT <mithradat@WSN-MIS-183.systemgroup.net>
2026-07-14 16:28:17 +0330

    completion of raw mode; ffi error handling

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ fn main() {
     let mut c = [0u8; 1];
     let mut stdin = io::stdin();
     while stdin.read(&mut c).unwrap() == 1 && c[0] != b'q' {
+        if c[0].is_ascii_control() {
+            println!("{}\r\n", c[0])
+        } else {
+            println!("{} -> {}\r\n", c[0], c[0] as char)
+        }
     }
 }
 
@@ -20,7 +25,9 @@ impl RawMode {
     pub fn new() -> Self {
         let mut termios = mem::MaybeUninit::<libc::termios>::uninit();
         unsafe {
-            libc::tcgetattr(libc::STDIN_FILENO, termios.as_mut_ptr());
+            if libc::tcgetattr(libc::STDIN_FILENO, termios.as_mut_ptr()) == -1 {
+                die_by_ffi("tcgetattr in RawMode::new()")
+            }
         }
         let raw = Self {
             original: unsafe {
@@ -33,15 +40,32 @@ impl RawMode {
     
     fn enable(&self) {
         let mut raw = self.original;
-        raw.c_lflag &= !libc::ECHO;
+        raw.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
+        raw.c_oflag &= !libc::OPOST;
+        raw.c_cflag |= libc::CS8;
+        raw.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
+        raw.c_cc[libc::VMIN] = 0;
+        raw.c_cc[libc::VTIME] = 1;
         unsafe {
-            libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &raw);
+            if libc::tcsetattr(
+                libc::STDIN_FILENO,
+                libc::TCSAFLUSH,
+                &raw)
+                == 1 {
+                    die_by_ffi("tcsetattr in RawMode::enable()")
+            }
         }
     }
 
     fn disable(&self) {
         unsafe {
-            libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &self.original);
+            if libc::tcsetattr(
+                libc::STDIN_FILENO,
+                libc::TCSAFLUSH,
+                &self.original)
+                == 1 {
+                    die_by_ffi("tcsetattr in RawMode::disable()")
+                }
         }
     }
 }
@@ -50,4 +74,9 @@ impl Drop for RawMode {
     fn drop(&mut self) {
         self.disable()
     }
+}
+
+
+fn die_by_ffi(msg: &str) -> ! {
+    panic!("{}", msg)
 }
