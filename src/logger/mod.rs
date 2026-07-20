f96d58bb9c98306673302c0af3e6b2e161eb0131
MITHRADAT <miladlotfimadieh@gmail.com>
2026-07-20 11:48:11 +0330

    read cusrsor position at bottom right of the terminal

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -6,7 +6,7 @@ pub struct Logger;
 
 impl Logger {
     pub fn log(text: &str) {
-        let file_name = Local::now().format("%Y%m%d%H%M").to_string();
+        let file_name = Local::now().format("%Y.%m.%d.%H-%M").to_string();
         let log_dir_path = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("log");
         fs::create_dir_all(&log_dir_path).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{mem, io::{self, Read, Write}, sync::OnceLock};
+use std::{mem, io::{self, Read, Write}, sync::OnceLock, str};
 use libc;
 
 mod logger;
@@ -56,16 +56,7 @@ fn read_key() -> u8 {
     let mut stdin = io::stdin();
     loop {
         match stdin.read(&mut c) {
-            Ok(1) => {
-                let text: String;
-                if c[0].is_ascii_control() {
-                    text = format!("you entered ascii control: {} -> {}", c[0], c[0] as char);
-                } else {
-                    text = format!("you entered: {}", c[0] as char);
-                }
-                Logger::log(text.as_str());
-                return c[0]
-            },
+            Ok(1) => return c[0],
             Ok(_) => continue,
             Err(err) => die(DieReason::Panic(err.to_string()))
         }
@@ -152,30 +143,62 @@ impl Drop for RawMode {
 }
 
 fn window_size() -> (u16, u16) {
-    let mut window = mem::MaybeUninit::<libc::winsize>::uninit();
-    unsafe {
-        if libc::ioctl(
-            libc::STDOUT_FILENO,
-            libc::TIOCGWINSZ,
-            window.as_mut_ptr()) >= 0 {
-            let win = window.assume_init();
-            if win.ws_row > 0 && win.ws_col > 0 {
-                return (win.ws_row, win.ws_col)
-            }
-        }
-    }
+    // let mut window = mem::MaybeUninit::<libc::winsize>::uninit();
+    // unsafe {
+    //     if libc::ioctl(
+    //         libc::STDOUT_FILENO,
+    //         libc::TIOCGWINSZ,
+    //         window.as_mut_ptr()) >= 0 {
+    //         let win = window.assume_init();
+    //         if win.ws_row > 0 && win.ws_col > 0 {
+    //             return (win.ws_row, win.ws_col)
+    //         }
+    //     }
+    // }
     cursor_position()
 }
 
 fn cursor_position() -> (u16, u16) {
-    Logger::log("cursor_position started");
     print!("\x1b[999C\x1b[999B");
-    // print!("\x1b[6n");
+    print!("\x1b[6n");
     flush();
-    read_key();
-    // println!();
-    Logger::log("cursor_position ended");
-    die(DieReason::FFI("cant get the size of window".to_string()))
+
+    let mut buff = [0u8; 32];
+    let mut stdin = io::stdin();
+    let mut i = 0;
+    loop {
+        match stdin.read(&mut buff[i..i + 1]) {
+            Ok(1) => {
+                if buff[i] == b'R' {
+                    break;
+                }
+                if buff[i].is_ascii_control() {
+                    Logger::log(format!("control({}): {} -> {:?}", i, buff[i], buff[i] as char).as_str());
+                } else {
+                    Logger::log(format!("usual({}): {}", i, buff[i] as char).as_str())
+                }
+                i += 1;
+            },
+            Ok(_) => {},
+            Err(err) => die(DieReason::Panic(err.to_string())) 
+        }
+    }
+
+    let response = str::from_utf8(&buff[2..i]).unwrap_or_else(|err| {
+        die(DieReason::Panic(err.to_string()))
+    });
+    Logger::log(format!("raw form of cursor position: {}", response).as_str());
+    let (rows, cols) = response.split_once(';').unwrap_or_else(|| {
+        die(DieReason::Panic("cant split the row and col from cursor position".to_string()))
+    });
+    let rows: u16 = rows.parse().unwrap_or_else(|_| {
+        die(DieReason::Panic("cant parse row from cursor position".to_string()))
+    });
+    let cols: u16 = cols.parse().unwrap_or_else(|_| {
+        die(DieReason::Panic("cant parse col from cursor position".to_string()))
+    });
+    Logger::log(format!("positionn from cusrsor: ({}, {})", rows, cols).as_str());
+    (rows, cols)
 }
 
 fn die(reason: DieReason) -> ! {
