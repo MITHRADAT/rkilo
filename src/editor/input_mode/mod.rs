b23007af09ed0df1c24fa7306a4a1007c8e23012
MITHRADAT <miladlotfimadieh@gmail.com>
2026-08-27 12:03:53 +0330

    remove Editor dependency from InputMode methods

--- src/editor/input_mode/mod.rs
+++ src/editor/input_mode/mod.rs
@@ -12,213 +12,254 @@ struct Prompt {
     prompt_type: PromptType
 }
 
+
 pub trait InputMode {
-    fn cursor_position(&self, editor: &Editor) -> (usize, usize);
-    fn scroll(&self, editor: &mut Editor);
-    fn write(&self, editor: &mut Editor, c: char);
-    fn move_cursor(&self, editor: &mut Editor, key: MoveKey);
-    fn enter_pressed(&self, editor: &mut Editor);
-    fn delete_pressed(&self, editor: &mut Editor);
-    fn backspace_pressed(&self, editor: &mut Editor);
+    fn cursor_position(&self)   -> impl Fn(&Editor) -> (usize, usize);
+    fn scroll(&self)            -> impl Fn(&mut Editor);
+    fn write(&self)             -> impl Fn(&mut Editor, char);
+    fn move_cursor(&self)       -> impl Fn(&mut Editor, MoveKey);
+    fn enter_pressed(&self)     -> impl Fn(&mut Editor);
+    fn delete_pressed(&self)    -> impl Fn(&mut Editor);
+    fn backspace_pressed(&self) -> impl Fn(&mut Editor);
 }
 
 impl InputMode for Normal {
-    fn cursor_position(&self, editor: &Editor) -> (usize, usize) {
-        let x =  editor.x_render() - editor.cursor.x_offset + 1;
-        let y = editor.cursor.y - editor.cursor.y_offset + 1;
-        (x, y)
+    fn cursor_position(&self) -> impl Fn(&Editor) -> (usize, usize) {
+        let function = |editor: &Editor| {
+            let x =  editor.x_render() - editor.cursor.x_offset + 1;
+            let y = editor.cursor.y - editor.cursor.y_offset + 1;
+            (x, y)
+        };
+        function
     }
 
-    fn scroll(&self, editor: &mut Editor) {
-        let x_render = editor.x_render();
-
-        if editor.cursor.y < editor.cursor.y_offset {
-            editor.cursor.y_offset = editor.cursor.y
-        }
-
-        if editor.cursor.y >= editor.cursor.y_offset + editor.screen.rows() {
-            editor.cursor.y_offset = editor.cursor.y - editor.screen.rows() + 1;
-        }
-
-        if x_render < editor.cursor.x_offset {
-            editor.cursor.x_offset = x_render
-        }
-
-        if x_render >= editor.cursor.x_offset + editor.screen.cols() {
-            editor.cursor.x_offset = x_render - editor.screen.cols() + 1;
-        }
-    }
-
-    fn write(&self, editor: &mut Editor, c: char) {
-        let cursor_y = editor.cursor.y;
-        if editor.file.lines.len() > cursor_y {
+    fn scroll(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
             let x_render = editor.x_render();
-            let cursor_x = editor.cursor.x;
-            let line = &mut editor.file.lines[cursor_y];
-            if line.chars.len() > cursor_x {
-                line.insert(c, cursor_x, x_render);
-            } else {
-                line.push(c);
+
+            if editor.cursor.y < editor.cursor.y_offset {
+                editor.cursor.y_offset = editor.cursor.y
             }
-        } else {
-            let line: String = (c).into();
-            editor.file.add_new_line(&line, true);
-        }
-    }
 
-    fn move_cursor(&self, editor: &mut Editor, key: MoveKey) {
-        match key {
-            MoveKey::ArrowUp => {
-                if editor.cursor.y > 0 {
-                    editor.cursor.y -= 1;
-                }
-                editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
-            },
-            MoveKey::ArrowDown => {
-                if editor.cursor.y + 1 < editor.file.lines.len() {
-                    editor.cursor.y += 1;
-                }
-                editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
-            },
-            MoveKey::ArrowLeft => {
-                if editor.cursor.x > 0 {
-                    editor.cursor.x -= 1;
-                } else if editor.cursor.y > 0 {
-                    editor.cursor.y -= 1;
-                    editor.cursor.x = editor.max_x();
-                }
-                editor.cursor.horizon = editor.cursor.x;
-            },
-            MoveKey::ArrowRight => {
-                if editor.cursor.x < editor.max_x() {
-                    editor.cursor.x += 1;
-                } else if editor.cursor.y + 1 < editor.file.lines.len() {
-                    editor.cursor.y += 1;
-                    editor.cursor.x = 0;
-                }
-                editor.cursor.horizon = editor.cursor.x;
-            },
-            MoveKey::PageUp => {
-                if editor.cursor.y >= editor.screen.rows() {
-                    editor.cursor.y -= editor.screen.rows()
-                } else {
-                    editor.cursor.y = 0;
-                }
-                editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
-            },
-            MoveKey::PageDown => {
-                if editor.cursor.y + editor.screen.rows() < editor.file.lines.len() {
-                    editor.cursor.y += editor.screen.rows()
-                } else {
-                    editor.cursor.y = editor.file.lines.len().saturating_sub(1)
-                }
-                editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
-            },
-            MoveKey::Home => {
-                editor.cursor.x = 0;
-                editor.cursor.horizon = 0
-            },
-            MoveKey::End => {
-                let max_x = editor.max_x();
-                editor.cursor.x = max_x;
-                editor.cursor.horizon = max_x
+            if editor.cursor.y >= editor.cursor.y_offset + editor.screen.rows() {
+                editor.cursor.y_offset = editor.cursor.y - editor.screen.rows() + 1;
             }
-        }
+
+            if x_render < editor.cursor.x_offset {
+                editor.cursor.x_offset = x_render
+            }
+
+            if x_render >= editor.cursor.x_offset + editor.screen.cols() {
+                editor.cursor.x_offset = x_render - editor.screen.cols() + 1;
+            }
+        };
+        function
     }
 
-    fn enter_pressed(&self, editor: &mut Editor) {
-        editor.file.split_line(editor.cursor.y, editor.cursor.x);
-        self.move_cursor(editor, MoveKey::ArrowRight);
-    }
-
-    fn delete_pressed(&self, editor: &mut Editor) {
-        let max_x = editor.max_x();
-        if editor.cursor.x == max_x && editor.cursor.y + 1 == editor.file.lines.len() {
-            return
-        }
-
-        if editor.cursor.x == max_x {
-            editor.file.merge_lines(editor.cursor.y + 1, editor.cursor.y);
-            return
-        }
-
-        editor.file.lines[editor.cursor.y].remove(editor.cursor.x)
-    }
-
-    fn backspace_pressed(&self, editor: &mut Editor) {
-        if editor.cursor.x == 0 && editor.cursor.y == 0 {
-            return
-        }
-
-        if editor.cursor.x == 0 {
+    fn write(&self) -> impl Fn(&mut Editor, char) {
+        let function = |editor: &mut Editor, c: char| {
             let cursor_y = editor.cursor.y;
-            self.move_cursor(editor, MoveKey::ArrowLeft);
-            editor.file.merge_lines(cursor_y, editor.cursor.y);
-            return
-        }
+            if editor.file.lines.len() > cursor_y {
+                let x_render = editor.x_render();
+                let cursor_x = editor.cursor.x;
+                let line = &mut editor.file.lines[cursor_y];
+                if line.chars.len() > cursor_x {
+                    line.insert(c, cursor_x, x_render);
+                } else {
+                    line.push(c);
+                }
+            } else {
+                let line: String = (c).into();
+                editor.file.add_new_line(&line, true);
+            }
+        };
+        function
+    }
 
-        self.move_cursor(editor, MoveKey::ArrowLeft);
-        editor.file.lines[editor.cursor.y].remove(editor.cursor.x);
-        return
+    fn move_cursor(&self) -> impl Fn(&mut Editor, MoveKey) {
+        let function = |editor: &mut Editor, key: MoveKey| {
+            match key {
+                MoveKey::ArrowUp => {
+                    if editor.cursor.y > 0 {
+                        editor.cursor.y -= 1;
+                    }
+                    editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
+                },
+                MoveKey::ArrowDown => {
+                    if editor.cursor.y + 1 < editor.file.lines.len() {
+                        editor.cursor.y += 1;
+                    }
+                    editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
+                },
+                MoveKey::ArrowLeft => {
+                    if editor.cursor.x > 0 {
+                        editor.cursor.x -= 1;
+                    } else if editor.cursor.y > 0 {
+                        editor.cursor.y -= 1;
+                        editor.cursor.x = editor.max_x();
+                    }
+                    editor.cursor.horizon = editor.cursor.x;
+                },
+                MoveKey::ArrowRight => {
+                    if editor.cursor.x < editor.max_x() {
+                        editor.cursor.x += 1;
+                    } else if editor.cursor.y + 1 < editor.file.lines.len() {
+                        editor.cursor.y += 1;
+                        editor.cursor.x = 0;
+                    }
+                    editor.cursor.horizon = editor.cursor.x;
+                },
+                MoveKey::PageUp => {
+                    if editor.cursor.y >= editor.screen.rows() {
+                        editor.cursor.y -= editor.screen.rows()
+                    } else {
+                        editor.cursor.y = 0;
+                    }
+                    editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
+                },
+                MoveKey::PageDown => {
+                    if editor.cursor.y + editor.screen.rows() < editor.file.lines.len() {
+                        editor.cursor.y += editor.screen.rows()
+                    } else {
+                        editor.cursor.y = editor.file.lines.len().saturating_sub(1)
+                    }
+                    editor.cursor.x = cmp::min(editor.cursor.horizon, editor.max_x())
+                },
+                MoveKey::Home => {
+                    editor.cursor.x = 0;
+                    editor.cursor.horizon = 0
+                },
+                MoveKey::End => {
+                    let max_x = editor.max_x();
+                    editor.cursor.x = max_x;
+                    editor.cursor.horizon = max_x
+                }
+            }
+        };
+        function
+    }
+
+    fn enter_pressed(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
+            editor.file.split_line(editor.cursor.y, editor.cursor.x);
+            self.move_cursor()(editor, MoveKey::ArrowRight);
+        };
+        function
+    }
+
+    fn delete_pressed(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
+            let max_x = editor.max_x();
+            if editor.cursor.x == max_x && editor.cursor.y + 1 == editor.file.lines.len() {
+                return
+            }
+
+            if editor.cursor.x == max_x {
+                editor.file.merge_lines(editor.cursor.y + 1, editor.cursor.y);
+                return
+            }
+
+            editor.file.lines[editor.cursor.y].remove(editor.cursor.x)
+        };
+        function
+    }
+
+    fn backspace_pressed(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
+            if editor.cursor.x == 0 && editor.cursor.y == 0 {
+                return
+            }
+
+            if editor.cursor.x == 0 {
+                let cursor_y = editor.cursor.y;
+                self.move_cursor()(editor, MoveKey::ArrowLeft);
+                editor.file.merge_lines(cursor_y, editor.cursor.y);
+                return
+            }
+
+            self.move_cursor()(editor, MoveKey::ArrowLeft);
+            editor.file.lines[editor.cursor.y].remove(editor.cursor.x);
+            return
+        };
+        function
     }
 
 }
 
 impl InputMode for Prompt {
-    fn cursor_position(&self, editor: &Editor) -> (usize, usize) {
-        let x = editor.cursor.x + 1;
-        let y = editor.screen.rows() + 2;
-        (x, y)
+    fn cursor_position(&self) -> impl Fn(&Editor) -> (usize, usize) {
+        let function = |editor: &Editor| {
+            let x = editor.cursor.x + 1;
+            let y = editor.screen.rows() + 2;
+            (x, y)
+        };
+        function
     }
 
-    fn scroll(&self, _: &mut Editor) {
-        return
+    fn scroll(&self) -> impl Fn(&mut Editor) {
+        let function = |_: &mut Editor| {};
+        function
     }
 
-    fn write(&self, editor: &mut Editor, c: char) {
-        if c == '\t' {
-            return
-        }
-        editor.status_bar.prompt_insert(c, editor.cursor.x);
+    fn write(&self) -> impl Fn(&mut Editor, char) {
+        let function = |editor: &mut Editor, c: char| {
+            if c == '\t' {
+                return
+            }
+            editor.status_bar.prompt_insert(c, editor.cursor.x);
+        };
+        function
     }
 
-    fn move_cursor(&self, editor: &mut Editor, key: MoveKey) {
-        match key {
-            MoveKey::ArrowLeft => {
-                if editor.status_bar.prompt_index(editor.cursor.x) > 0 {
-                    editor.cursor.x -= 1;
-                }
-            },
-            MoveKey::ArrowRight => {
-                if editor.cursor.x < editor.message().len() {
-                    editor.cursor.x += 1;
-                }
-            },
-            MoveKey::Home => {
-                editor.cursor.x = 0
-            },
-            MoveKey::End => {
-                editor.cursor.x = editor.max_x()
-            },
-            MoveKey::ArrowUp   => { return },
-            MoveKey::ArrowDown => { return },
-            MoveKey::PageUp    => { return },
-            MoveKey::PageDown  => { return },
-        }
+    fn move_cursor(&self) -> impl Fn(&mut Editor, MoveKey) {
+        let function = |editor: &mut Editor, key: MoveKey| {
+            match key {
+                MoveKey::ArrowLeft => {
+                    if editor.status_bar.prompt_index(editor.cursor.x) > 0 {
+                        editor.cursor.x -= 1;
+                    }
+                },
+                MoveKey::ArrowRight => {
+                    if editor.cursor.x < editor.message().len() {
+                        editor.cursor.x += 1;
+                    }
+                },
+                MoveKey::Home => {
+                    editor.cursor.x = 0
+                },
+                MoveKey::End => {
+                    editor.cursor.x = editor.max_x()
+                },
+                MoveKey::ArrowUp   => { return },
+                MoveKey::ArrowDown => { return },
+                MoveKey::PageUp    => { return },
+                MoveKey::PageDown  => { return },
+            }
+        };
+        function
     }
 
-    fn enter_pressed(&self, editor: &mut Editor) {
-        self.commit(editor);
+    fn enter_pressed(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
+            self.commit(editor);
+        };
+        function
     }
 
-    fn delete_pressed(&self, editor: &mut Editor) {
-        editor.status_bar.prompt_delete(editor.cursor.x);
+    fn delete_pressed(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
+            editor.status_bar.prompt_delete(editor.cursor.x);
+        };
+        function
     }
 
-    fn backspace_pressed(&self, editor: &mut Editor) {
-        if editor.status_bar.prompt_backspace(editor.cursor.x).is_some() {
-            self.move_cursor(editor, MoveKey::ArrowLeft)
-        }
+    fn backspace_pressed(&self) -> impl Fn(&mut Editor) {
+        let function = |editor: &mut Editor| {
+            if editor.status_bar.prompt_backspace(editor.cursor.x).is_some() {
+                self.move_cursor()(editor, MoveKey::ArrowLeft)
+            }
+        };
+        function
     }
 
 }
